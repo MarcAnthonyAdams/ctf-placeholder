31f82328df1e48207321295c929d964030de5697
Marc adams <marc.adams1982@gmail.com>
2022-01-18 18:27:46 -0600

    Third Commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="display">
         <span className="displayLogo">
-            <img className="displayLogoImg" src="/ConnectTF-logo-1.png" alt="" />
+            <img className="displayLogoImg" src="images/ConnectTF-logo-1.png" alt="" />
         </span>
         <span className="displayDescription">A place where those who are on the <b style={{color: "#e639af"}}>twin flame</b> journey can <b style={{color: "#4a76fd"}}>connect</b>!</span>
         <span className="comingSoon"><b style={{color: "#4a76fd"}}>Coming</b> <b style={{color: "#e639af"}}>Soon!</b></span>
